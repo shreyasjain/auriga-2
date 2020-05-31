4f51f60bc64bac7072c958f6f4e3974f601041c2
shreyasjain <13sjain4ur7@gmail.com>
2020-05-31 19:16:04 +0530

    tabs modified

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ function tabFunctioning(event,container,id,anchorId){
     const anchors = document.getElementsByTagName("a")
     for(let i=0;i<anchors.length;i++){
         anchors[i].style.textDecoration="underline"
+        anchors[i].style.borderBottom="1px solid darkgray"
     }
     document.getElementById(anchorId).style.textDecoration = "none"
+    document.getElementById(anchorId).style.borderBottom = "1px solid #bfc0bf"
 
     const containerElements = document.getElementsByClassName(container)
     for(let i=0;i<containerElements.length;i++){
