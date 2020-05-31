7b9baa3ff4b522cb96bfc66b81123974111b6743
shreyasjain <13sjain4ur7@gmail.com>
2020-05-31 11:48:14 +0530

    button issue resolved

---
+++ app.js
@@ -0,0 +1,16 @@
+function tabFunctioning(event,container,id,anchorId){
+    event.preventDefault()
+
+    const anchors = document.getElementsByTagName("a")
+    for(let i=0;i<anchors.length;i++){
+        anchors[i].style.textDecoration="none"
+    }
+    document.getElementById(anchorId).style.textDecoration = "underline"
+
+    const containerElements = document.getElementsByClassName(container)
+    for(let i=0;i<containerElements.length;i++){
+    containerElements[i].style.display="none"}
+
+    document.getElementById(id).style.display="block"
+
+}
